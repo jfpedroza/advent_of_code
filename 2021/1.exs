65edb5244e12f5a48022d657826c2b25eba4991c
Jhon Pedroza <jhon@pedroza.me>
2022-03-24 23:55:40 -0500

    Solve AoC 2021 Day 1 Part 2 in Elixir

--- 2021/1.exs
+++ 2021/1.exs
@@ -3,4 +3,10 @@ input = IO.read(:eof) |> String.split("\n", trim: true) |> Enum.map(&String.to_i
 input
 |> Stream.zip(tl(input))
 |> Enum.count(fn {a, b} -> a < b end)
-|> IO.inspect()
+|> IO.inspect(label: "Part 1")
+
+Stream.zip([input, tl(input), tl(tl(input))])
+|> Enum.map(fn {a, b, c} -> a + b + c end)
+|> then(&Stream.zip(&1, tl(&1)))
+|> Enum.count(fn {a, b} -> a < b end)
+|> IO.inspect(label: "Part 2")
