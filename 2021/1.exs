48f81d7c77a341b760661335e2cd6a2a9009d731
Jhon Pedroza <jhon@pedroza.me>
2022-03-25 00:34:23 -0500

    Refactor Aoc 2021 Day 1 to use more streams

--- 2021/1.exs
+++ 2021/1.exs
@@ -1,12 +1,13 @@
-input = IO.read(:eof) |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1)
+input = IO.stream() |> Stream.map(&String.trim/1) |> Enum.map(&String.to_integer/1)
 
 input
-|> Stream.zip(tl(input))
+|> Stream.zip(Stream.drop(input, 1))
 |> Enum.count(fn {a, b} -> a < b end)
 |> IO.inspect(label: "Part 1")
 
-Stream.zip([input, tl(input), tl(tl(input))])
-|> Enum.map(fn {a, b, c} -> a + b + c end)
-|> then(&Stream.zip(&1, tl(&1)))
+0..2
+|> Stream.map(&Stream.drop(input, &1))
+|> Stream.zip_with(fn [a, b, c] -> a + b + c end)
+|> then(&Stream.zip(&1, Stream.drop(&1, 1)))
 |> Enum.count(fn {a, b} -> a < b end)
 |> IO.inspect(label: "Part 2")
